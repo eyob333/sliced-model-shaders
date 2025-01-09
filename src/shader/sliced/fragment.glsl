c89363e356d6781e9a66c1bee586613fe23555f6
eyob j <jemijobs22@gmail.com>
2025-01-09 11:06:03 +0300

    feat: add ark

--- src/shader/sliced/fragment.glsl
+++ src/shader/sliced/fragment.glsl
@@ -1,5 +1,14 @@
 varying vec3 vPosition;
 
 void main(){
-    csm_FragColor = vec4(vPosition, 1.0);
+    float uSliceStart = 1.0;
+    float uSliceArc = 1.5;
+
+    // angle
+    float angle = atan(vPosition.y, vPosition.x );
+    angle -= uSliceStart;
+
+    if ( angle > 0. && angle < uSliceArc)
+        discard;
+    csm_FragColor = vec4(vec3(angle), 1.0);
 }
